80a70b3b93cf2d636718d7d174b73a9828d8639f
Souleymeine <souleymeine@gmail.com>
2025-04-29 14:14:45 +0200

    Centered, 8 bit gradient-colored label with adjustable center offset
    The program enables the alternate buffer, hides the cursor, disables the
    terminal's echo, displays the number of rows and columns, the colors the
    entire grid with alternating colors to ease the counting of the cells.
    It then determines the propert left offset in order to print a label of
    variable length (which is the first argument of the program or "Hello
    World!" is there is none) in the center of the terminal, according to
    the given horizontal alignment margin of error, if the label cannot be
    perfectly centered. Also note that the label uses a gradient of 8 bit
    color values from "black" to "white" (refer to ANSI specification for
    this). It then determines the right row according to the specified
    vertical offset margin of error, if the label cannot be perfectly
    virticaly centered.
    The program then waits for the enter key to be pressed and restores the
    terminal's original state (re-enable echo, show normal buffer and
    cursor).

--- src/test.c
+++ src/test.c
@@ -1,13 +1,70 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <termios.h>
 
-int main() {
-	// On Saturday, the 19th of April 2025.
-	printf(
-		"\x1b[?1049h\x1b[H"
-		"\x1b[3mThe excitement of creating something new and hopefully useful flows down your veins\n"
-		"as you prepare for the very first commit.\x1b[23m\n\n\n\n"
-		"\x1b[1;33m		You are filled with determination.\x1b[m");
+#define max(x, y) x <= y ? x : y
+
+enum horiz_margin_of_err : bool {
+	LEFT, RIGHT
+};
+enum virt_margin_of_err : bool {
+	TOP, BOTTOM
+};
+#define HORIZ_MARGIN_OF_ERR_ALIGN LEFT
+#define VIRT_MARGIN_OF_ERR_ALIGN BOTTOM
+
+int main(int argc, char **argv) {
+	printf("\x1b[?1049h"); // Shows alternate buffer
+	printf("\x1b[?25l"); // Hides the cursor
+
+	struct termios term_attr;
+	tcgetattr(STDIN_FILENO, &term_attr);
+	term_attr.c_lflag &= ~ECHO;
+	tcsetattr(STDIN_FILENO, 0, &term_attr);
+
+	struct winsize window;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &window); // Get the window width and height in cells (columns, lines) and in pixels
+
+	// Paint half the cells (for visualization purposes)
+	for (int i = 0; i < window.ws_row * window.ws_col; ++i) {
+		printf(i % 2 == 0 ? "\x1b[48;5;232m \x1b[m" : " ");
+	}
+	printf("\x1b[H"); // move cursor to home position
+
+	printf("cols : %hu, rows: %hu\n", window.ws_col, window.ws_row);
+
+	const char* label = (argc == 1 ? "Hello, World!" : argv[1]);
+	const int label_len = strlen(label);
+
+	// When the parity of the label's length and the number of columns isn't the same, a slight offset will be produced
+	// either left or right, due to the nature of the integer division.
+	// We look for it and check if the HORIZ_MARGIN_OF_ERR_ALIGN property is opposite to the offset
+	// We can the apply a correction of + 1 or - 1 to match the desired off-centered alignment
+	unsigned short left_gap_len = (window.ws_col / 2) - (label_len / 2); 
+	const unsigned short right_gap_len = window.ws_col - (left_gap_len + label_len);
+	const char align_error = left_gap_len - right_gap_len; // -1 if left-centered, 1 if right-centered, 0 if perfectly centered
+	if (align_error == -1 && HORIZ_MARGIN_OF_ERR_ALIGN == RIGHT) {
+		left_gap_len += 1;
+	} else if (align_error == 1 && HORIZ_MARGIN_OF_ERR_ALIGN == LEFT) {
+		left_gap_len -= 1;
+	}
+
+	const unsigned short virt_margin_of_error = ((window.ws_row % 2) != 0 ? 1 : VIRT_MARGIN_OF_ERR_ALIGN);
+	const unsigned short middle_line = window.ws_row / 2 + virt_margin_of_error;
+
+	printf("\x1b[%hu;%huH", middle_line, left_gap_len + 1); // We go to y, x (line, column)
+	for (int i = 0; i < label_len; ++i) {
+		uint8_t val = max(232 + ((float)i / label_len) * 24, 255); // 8 bit color ID
+		printf("\x1b[38;5;%hhu;1;3m%c\x1b[m", val, label[i]); // Bold, italic and grayscaled gradient colored
+	}
+
 	getchar();
-	printf("\x1b[?1049l");
+	printf("\x1b[?1049l"); // Hides the alternate buffer
+	printf("\x1b[?25h"); // Shows the cursor
+	term_attr.c_lflag |= ECHO;
+	tcsetattr(STDIN_FILENO, 0, &term_attr);
 }
 
