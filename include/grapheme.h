986d75cdf1e3d339cd878d234e7f12eb7b23e225
Souleymeine <souleymeine@gmail.com>
2025-10-09 00:35:39 +0200

    Simplify identifiers and tighten logic
    Adding utf8 to everything felt heavy, as the concept of "grapheme" for
    exemple, is dedicated to such encoding. It made "sense" to imply we're
    only talking about utf8.
    The 3rd statement of the foor loop is now used to increment both i and
    count.

--- include/grapheme.h
+++ include/grapheme.h
@@ -4,9 +4,11 @@
 #include <uchar.h>
 
 /**
- * An enum which should be used to describe the role of a code point in a utf8 string.
+ * Describes the role of a codepoint (aka 'cp') in a utf8 string.
+ * This enum can be used to process utf8 strings grapheme by grapheme instead of one 'cp' at a time.
+ * try the fonction `count_graphemes`.
  */
-enum utf8cpt_type : char {
+enum cptype : char {
 	INVALID = -1,
 	CONTINUATION,
 	ASCII,
@@ -19,11 +21,11 @@ enum utf8cpt_type : char {
  * Return the corresponding `enum cpt_type` for the given character.
  * Defaults to `INVALID` if none of the conditions are met.
  */
-enum utf8cpt_type get_utf8cpt_type(const char8_t c);
+enum cptype get_cptype(char8_t c);
 
 /**
  * Count the number of graphemes with the given utf8 string.
  * Returns -1 if the string is not valid utf8.
  */
-int count_utf8_graphemes(const char* restrict str, const size_t str_len);
+size_t count_graphemes(const char* restrict str, size_t strlen);
 
--- src/grapheme.c
+++ src/grapheme.c
@@ -1,17 +1,17 @@
 #include <assert.h>
 #include <stdbit.h>
+#include <stdio.h>
 #include <uchar.h>
 
 #include "grapheme.h"
 
-enum utf8cpt_type get_utf8cpt_type(const char8_t c)
+enum cptype get_cptype(char8_t c)
 {
-	// https://www.rfc-editor.org/rfc/rfc3629#section-3
+	// see https://www.rfc-editor.org/rfc/rfc3629#section-3
 	if (stdc_bit_width_uc(c) < 8u) {
 		return ASCII;
 	} else {
 		const unsigned char leading_ones = stdc_leading_ones_uc(c);
-		assert(leading_ones <= 4u);
 		if (stdc_first_leading_zero_uc(c) == leading_ones + 1u) {
 			return leading_ones;
 		}
@@ -20,19 +20,14 @@ enum utf8cpt_type get_utf8cpt_type(const char8_t c)
 	return INVALID;
 }
 
-int count_utf8_graphemes(const char* restrict str, const size_t str_len)
+size_t count_graphemes(const char* restrict str, size_t strlen)
 {
 	unsigned int count = 0;
 
-	char i_type = INVALID;
-	size_t i    = 0;
-	while (i < str_len) {
-		i_type = get_utf8cpt_type(str[i]);
-		if (i_type == INVALID) {
+	enum cptype type = INVALID;
+	for (size_t i = 0; i < strlen; i += type, ++count) {
+		if ((type = get_cptype(str[i])) == INVALID) {
 			return -1;
-		} else {
-			++count;
-			i += i_type;
 		}
 	}
 
