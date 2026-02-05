1117b1f8f91bca6aeec7b33506853950bcd9a6ac
Souleymeine <souleymeine@gmail.com>
2026-02-05 18:16:52 +0100

    Add splat_gradient test

--- test/gradient.c
+++ test/gradient.c
@@ -1,4 +1,3 @@
-#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -11,21 +10,15 @@ int main(int argc, char* argv[])
 	const char* gphm = (argc == 1) ? "é" : argv[1];
 
 	const struct termsize size = get_termsize();
-	const float increment      = 0.05f;
-	float progress             = 0;
-	do {
-		for (uint16_t y = 1; y <= size.rows; ++y) {
-			for (uint16_t x = 1; x <= size.cols; ++x) {
-				setgphm(gphm, x, y);
-				setclrpair(CLR_RGB(x * 255 / size.cols, y * 255 / size.rows, ((sin(progress) + 1) / 2) * 255),
-				           CLR_CODE(CLRCODE_YELLOW), x, y);
-			}
+	for (uint16_t y = 1; y <= size.rows; ++y) {
+		for (uint16_t x = 1; x <= size.cols; ++x) {
+			setgphm(gphm, x, y);
+			setclrpair(CLR_RGB(x * 255 / size.cols, y * 255 / size.rows, 0), CLR_CODE(CLRCODE_CYAN), x, y);
 		}
-		progress += increment;
-		refresh();
-	} while (getchar() != '\x1b');
+	}
+	refresh();
 
-
+	getchar();
 	cleanup_term();
 }
 
---
+++ test/splat_gradient.c
@@ -0,0 +1,30 @@
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "../include/screen.h"
+#include "../include/terminal.h"
+
+int main(int argc, char* argv[])
+{
+	init_flags(TERM_NO_ECHO | TERM_HIDE_CURSOR | TERM_ALTBUF);
+	const char* gphm = (argc == 1) ? "é" : argv[1];
+
+	const struct termsize size = get_termsize();
+	const float increment      = 0.05f;
+	float progress             = 0;
+	do {
+		for (uint16_t y = 1; y <= size.rows; ++y) {
+			for (uint16_t x = 1; x <= size.cols; ++x) {
+				setgphm(gphm, x, y);
+				setclrpair(CLR_RGB(x * 255 / size.cols, y * 255 / size.rows, ((sin(progress) + 1) / 2) * 255),
+				           CLR_CODE(CLRCODE_YELLOW), x, y);
+			}
+		}
+		progress += increment;
+		refresh();
+	} while (getchar() != '\x1b');
+
+
+	cleanup_term();
+}
