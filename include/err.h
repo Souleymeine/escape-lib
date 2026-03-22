2d37adbb31bdf45e4a3884adbe9aa299b556fc61
Souleymeine <souleymeine@gmail.com>
2026-03-22 14:37:32 +0100

    WIP : Remove footguns introduced by the first iteration of err.h
    The declaration of result types was weird as it duplicates the
    definition. That alone may cause subtle bugs across TUs and overall
    isn't explicit enough, although I like the "lazy" type declaration
    ngl...
    Now, structs enums and unions have to declare their own result type and
    result types for standard C types are declared upfront in err.h

--- include/err.h
+++ include/err.h
@@ -1,56 +1,11 @@
 #pragma once
 
-/**
- * escres_T is a way to give the struct a unique name for each T but is not intended to be written as such,
- * altough nothing prevents you from writing
- * escres_int32_t instead of ESC_RESULT(int32_t) or
- * escres_struct_foo instead of ESC_RESULT(struct foo).
- */
-#define ESC_RESULT(T) struct ESC_RESULT_TYPENAME(T) { \
-	const enum escerror err;                          \
-	_ESC_IF_NOT_VOID(T)(const T val;)()               \
-}
-#define ESC_RESULT_PTR(T) struct ESC_RESULT_TYPENAME_PTR(T) { \
-	const enum escerror err;                                  \
-	T* val;                                                   \
-}
+#include <stddef.h>
+#include <uchar.h>
 
-#define ESC_RES_VAL(T, v) (struct ESC_RESULT_TYPENAME(T)) {.val = v, .err = 0}
-#define ESC_RES_ERR(T, e) (struct ESC_RESULT_TYPENAME(T)) {.err = e}
-#define ESC_RES_NOERR(T)  (struct ESC_RESULT_TYPENAME(T)) {.err = 0}
-
-#define ESC_RESPTR_VAL(T, v) (struct ESC_RESULT_TYPENAME_PTR(T)) {.val = v, .err = 0}
-#define ESC_RESPTR_ERR(T, e) (struct ESC_RESULT_TYPENAME_PTR(T)) {.err = e}
-#define ESC_RESPTR_NOERR(T)  (struct ESC_RESULT_TYPENAME_PTR(T)) {.err = 0}
-
-#define ESC_TRY(T, res)     if (res.err) return ESC_RES_ERR(T, res.err)
-#define ESC_TRY_PTR(T, res) if (res.err) return ESC_RESPTR_ERR(T, res.err)
-
-#if __STDC_VERSION__ >= 202311L
-#define _ESC_NODISCARD(reason) [[nodiscard(reason)]]
-#elif
-#define _ESC_NODISCARD(reason)
-#endif
-
-#define ESC_OK(res) (!res.err)
-enum escerror : unsigned {
-// global
-	ESC_ERR_OUT_OF_MEMORY = 1,
-	ESC_ERR_CANNOT_FREE_MEMORY,
-// io
-// rndr
-	ESC_ERR_INVALID_UTF8_CU,
-	ESC_ERR_X_OUT_OF_BOUNDS,
-	ESC_ERR_Y_OUT_OF_BOUNDS,
-	ESC_ERR_XY_OUT_OF_BOUNDS,
-// 2d
-// img
-// 3d
-};
-
+// What you're looking for is probably below that big block
 
 /** ------ Macro magic start (scary) -----
- *
  * Thanks to raccoon7378 on the Zig programming language Discord Server!
  * original link : https://c.godbolt.org/z/TdvrYW53M
  * TODO : figure out how it works precisely */
@@ -70,19 +25,10 @@ enum escerror : unsigned {
 #define ___ESC_IS_VOID_void ___ESC_PROBE(~)
 #define _ESC_IF_NOT_VOID(T) ___ESC_IF(___ESC_ZERO(___ESC_IS_VOID(T)))
 // Thanks to holyblackcat on Discord!!
-/* 
- * ESC_TYPEID(x)          -> x
- * ESC_TYPEID(unsigned x) -> ux
- * ESC_TYPEID(struct x)   -> struct_x
- * ESC_TYPEID(union x)    -> union_x
- * ESC_TYPEID(enum x)     -> enum_x
- */
-// types up to 4 words such as unsigned long long int
 #define ESC_RESULT_TYPENAME(T) _ESC_CAT(escres_,ESC_TYPEID(T))
 #define ESC_RESULT_TYPENAME_PTR(T) _ESC_CAT(escres_,ESC_TYPEID_PTR(T))
-
 #define ESC_TYPEID_PTR(name) _ESC_CAT(ESC_TYPEID(name),_ptr)
-#define ESC_TYPEID(name) ___ESC_TYPEID_(___ESC_TYPEID_(___ESC_TYPEID_(___ESC_TYPEID_(name)))) 
+#define ESC_TYPEID(name) ___ESC_TYPEID_(___ESC_TYPEID_(___ESC_TYPEID_(___ESC_TYPEID_(name)))) // types up to 4 words such as unsigned long long int
 #define ___ESC_TYPEID_(name_) ___ESC_DETAIL_TYPEID_SELECT(___ESC_DETAIL_TYPEID_ANALYZE(name_), name_, ___ESC_DETAIL_TYPEID_ELABORATED, ___ESC_DETAIL_TYPEID_NOT_ELABORATED, x)
 
 #define ___ESC_DETAIL_TYPEID_ANALYZE(name_) ___ESC_DETAIL_TYPEID_ANALYZE_##name_
@@ -100,12 +46,10 @@ enum escerror : unsigned {
 #define ___ESC_DETAIL_TYPEID_ANALYZE_ulongint ulong,
 #define ___ESC_DETAIL_TYPEID_ANALYZE_ulonglong ulonglong,
 #define ___ESC_DETAIL_TYPEID_ANALYZE_ulonglongint ulonglong,
-
 #define ___ESC_DETAIL_TYPEID_ANALYZE_short short,
 #define ___ESC_DETAIL_TYPEID_ANALYZE_shortint short,
 #define ___ESC_DETAIL_TYPEID_ANALYZE_ushort ushort,
 #define ___ESC_DETAIL_TYPEID_ANALYZE_ushortint ushort,
-
 #if defined(_MSC_VER) && !defined(__clang__) && (!defined(_MSVC_TRADITIONAL) || _MSVC_TRADITIONAL == 1) // If using legacy MSVC preprocessor...
 #  define ___ESC_DETAIL_TYPEID_EMPTY
 #  define ___ESC_DETAIL_TYPEID_SELECT(...) ___ESC_DETAIL_TYPEID_SELECT_ ___ESC_DETAIL_TYPEID_EMPTY(__VA_ARGS__)
@@ -113,7 +57,88 @@ enum escerror : unsigned {
 #  define ___ESC_DETAIL_TYPEID_SELECT(...) ___ESC_DETAIL_TYPEID_SELECT_(__VA_ARGS__)
 #endif
 #define ___ESC_DETAIL_TYPEID_SELECT_(unused_or_prefix_, name_, unused_, func_, ...) func_(unused_or_prefix_, name_)
-
 #define ___ESC_DETAIL_TYPEID_NOT_ELABORATED(unused_, name_) name_
 #define ___ESC_DETAIL_TYPEID_ELABORATED(prefix_, name_) prefix_##name_
 /* ------ Macro magic end ------ */
+
+#if __STDC_VERSION__ >= 202311L
+#define _ESC_NODISCARD(reason) [[nodiscard(reason)]]
+#elif
+#define _ESC_NODISCARD(reason)
+#endif
+
+#define _ESC_RESULT_DECL(T) struct _ESC_NODISCARD("Error ignored") \
+ESC_RESULT_TYPENAME(T) {                      \
+	const enum escerror err;                  \
+	_ESC_IF_NOT_VOID(T)(const T val;)()       \
+}
+#define _ESC_RESULT_PTR_DECL(T) struct _ESC_NODISCARD("Pointer error ignored") \
+ESC_RESULT_TYPENAME_PTR(T) {                  \
+	const enum escerror err;                  \
+	T* val;                                   \
+}
+
+#define ESC_OK(res) (!res.err)
+enum escerror : unsigned {
+// global
+	ESC_ERR_OUT_OF_MEMORY = 1,
+	ESC_ERR_CANNOT_FREE_MEMORY,
+// io
+// rndr
+	ESC_ERR_INVALID_UTF8_CU,
+	ESC_ERR_X_OUT_OF_BOUNDS,
+	ESC_ERR_Y_OUT_OF_BOUNDS,
+	ESC_ERR_XY_OUT_OF_BOUNDS,
+// 2d
+// img
+// 3d
+};
+
+/**
+ * List of all the result type declarations for stdc types used in the library
+ * This is done upfront so _ESC_RESULT_DECL is only used for structs, enums and unions,
+ * placed after the declaration.
+ */
+_ESC_RESULT_PTR_DECL(char);
+_ESC_RESULT_PTR_DECL(char8_t);
+_ESC_RESULT_PTR_DECL(char32_t);
+_ESC_RESULT_PTR_DECL(void);
+_ESC_RESULT_DECL(void);
+_ESC_RESULT_DECL(size_t);
+_ESC_RESULT_DECL(char8_t);
+_ESC_RESULT_DECL(char32_t);
+
+#define ESC_RESULT(T)     struct ESC_RESULT_TYPENAME(T)
+#define ESC_RESULT_PTR(T) struct ESC_RESULT_TYPENAME_PTR(T)
+
+#define ESC_RES_VAL(T, v) (ESC_RESULT(T)) {.val = v, .err = 0}
+#define ESC_RES_ERR(T, e) (ESC_RESULT(T)) {.err = e}
+#define ESC_RES_NOERR(T)  (ESC_RESULT(T)) {.err = 0}
+
+#define ESC_RESPTR_VAL(T, v) (ESC_RESULT_PTR(T)) {.val = v, .err = 0}
+#define ESC_RESPTR_ERR(T, e) (ESC_RESULT_PTR(T)) {.err = e}
+#define ESC_RESPTR_NOERR(T)  (ESC_RESULT_PTR(T)) {.err = 0}
+
+#if __STDC_VERSION__ >= 202311L
+#define ESC_TRY(T, res)                                 \
+	do {                                                \
+		const auto r = res;                             \
+		if (r.err) return ESC_RES_ERR(T, r.err);        \
+	} while (false)
+#define ESC_TRY_PTR(T, res)                             \
+	do {                                                \
+		const auto r = res;                             \
+		if (r.err) return ESC_RESPTR_ERR(T, r.err);     \
+	} while (false)
+#else
+#define ESC_TRY(T, res)                                 \
+	do {                                                \
+		const __typeof(x)__(res) r = res;               \
+		if (r.err) return ESC_RES_ERR(T, r.err);        \
+	} while (false)
+#define ESC_TRY_PTR(T, res)                             \
+	do {                                                \
+		const __typeof(x)__(res) r = res;               \
+		if (r.err)   return ESC_RESPTR_ERR(T, r.err);   \
+	} while (false)
+#endif
