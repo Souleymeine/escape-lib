d256a32421dff5f7fd3c4a68ca0f362166e29a27
Souleymeine <souleymeine@gmail.com>
2026-03-24 00:31:40 +0100

    Fix tests

--- test/splat_gradient.c
+++ test/splat_gradient.c
@@ -1,32 +1,35 @@
 #include <math.h>
-#include <stdint.h>
 #include <stdio.h>
 
-#include "../include/screen.h"
-#include "../include/terminal.h"
-#include "../include/utf.h"
+#include "../include/rndr.h"
+#include "../include/core.h"
 
 int main(int argc, char* argv[])
 {
-	init_flags(TERM_NO_ECHO | TERM_HIDE_CURSOR | TERM_ALTBUF);
-	const char32_t cp = (argc == 1) ? U'é' : strtocp(UTF8(argv[1]));
+	esc_init();
+	(void)esc_initscr(ESC_STRBUF_IMPL_GROWABLE(2048, 1.25f), false, ESC_CLR_CODE(ESC_CLRCODE_DEF), ESC_CLR_CODE(ESC_CLRCODE_DEF));
+	(void)esc_settermflags(ESC_TERM_NO_ECHO | ESC_TERM_HIDE_CURSOR | ESC_TERM_ALTBUF);
+	
+	const char32_t cp = (argc == 1) ? U'é' : esc_mbtocp(ESC_UTF8(argv[1])).val;
 
-	const struct termsize size = get_termsize();
+	const struct esc_termsize size = esc_getsize();
 	const float increment      = 0.05f;
 	float progress             = 0;
 	do {
-		for (uint16_t y = 1; y <= size.rows; ++y) {
-			for (uint16_t x = 1; x <= size.cols; ++x) {
-				setcp(cp, x, y);
-				setclrpair(CLR_RGB(x * 255 / size.cols, y * 255 / size.rows, ((sin(progress) + 1) / 2) * 255),
-				           CLR_CODE(CLRCODE_YELLOW), x, y);
+		for (uint16_t y = 0; y < size.rows; y++) {
+			for (uint16_t x = 0; x < size.cols; x++) {
+				(void)esc_setcp(cp, x, y);
+				(void)esc_setclrpair(
+					ESC_CLR_RGB(x * 255 / size.cols, y * 255 / size.rows, ((sin(progress) + 1) / 2) * 255),
+					ESC_CLR_CODE(ESC_CLRCODE_YELLOW),
+					x, y
+				);
 			}
 		}
 		progress += increment;
-		refresh(false);
+		(void)esc_refresh(false);
 	} while (getchar() != '\x1b');
 
-
-	cleanup_term();
+	esc_cleanup();
 	return 0;
 }
